be3dcb553c17b8969bf011ebe2e5ca0b800a19d8
Roberto Tobias <r.tobias@hotmail.com.br>
2026-04-02 18:45:10 -0300

    Untrack dist and persist .gitignore

--- dist/index.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const orchestrator_1 = require("./agent/orchestrator");
-(0, orchestrator_1.runAgent)().catch((error) => {
-    const message = error instanceof Error ? error.stack || error.message : String(error);
-    console.error(message);
-    process.exit(1);
-});
